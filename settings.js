552615231c62d9815767758cbfec511fc0ef65e0
Sosy255 <167062367+Sosy255@users.noreply.github.com>
2024-07-29 23:56:25 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "255782365586"
+global.ownername = "🐛 mr tyrant "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
+global.ownernumber = '255782365586'  //creator number
+global.ownername = ' Mjamaica ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
 //sticker details
